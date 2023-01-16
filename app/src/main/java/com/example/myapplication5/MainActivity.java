a3eb401e48ff723c8d229ffde86f472270dc1338
Niklas <niklas@158-37-228-39.hvl.no>
2023-01-16 14:37:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication5/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+}
